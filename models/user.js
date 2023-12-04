409fbb4b9345038d7b0cc6cd61048927ab3e06f7
joaonative <jestudos127@gmail.com>
2023-12-03 23:28:03 -0300

    feat: user model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (pass) => pass.length >= 8,
+        message: "Password should be at least 8 characters long",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  const salt = bcrypt.genSaltSync(10);
+  user.password = bcrypt.hashSync(user.password, salt);
+  next();
+});
+
+export const User = mongoose.models.User || mongoose.model("User", UserSchema);

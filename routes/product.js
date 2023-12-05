1499e1be857fe40715d139eda9084547495b3cda
joaonative <jestudos127@gmail.com>
2023-12-05 16:50:05 -0300

    feat: new list routes

--- routes/product.js
+++ routes/product.js
@@ -3,11 +3,15 @@ import { authMiddleware } from "../middleware/authMiddleware.js";
 import {
   createProduct,
   editProduct,
+  listAllProducts,
+  listProductsByCategory,
 } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.put("/:id", authMiddleware, editProduct);
 router.post("/", authMiddleware, createProduct);
+router.get("/", listAllProducts);
+router.get("/:category", listProductsByCategory);
 
 export default router;

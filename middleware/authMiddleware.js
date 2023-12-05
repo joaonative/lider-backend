38477ae374d2cf4058295c64b8f26c12d4754ed5
joaonative <jestudos127@gmail.com>
2023-12-05 14:38:27 -0300

    feat: authentication middleware created

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export function authMiddleware(req, res, next) {
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Token não fornecido" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: "Token inválido" });
+    }
+
+    req.user = { id: decoded.userId };
+    next();
+  });
+}

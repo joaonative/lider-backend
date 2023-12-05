496e29cfadfa1111d5001ab2b7b6dc28daa7febb
joaonative <jestudos127@gmail.com>
2023-12-05 14:38:42 -0300

    feat: new model in db review

---
+++ models/review.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    user: {
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      email: { type: String, required: true },
+    },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    message: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Review =
+  mongoose.models.Review || mongoose.model("Review", ReviewSchema);

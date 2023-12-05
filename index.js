fe04d1130680ab339a629e58735161e952a2c51d
joaonative <jestudos127@gmail.com>
2023-12-05 14:37:57 -0300

    feat: using authmiddleware and fix db connection

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import registerRoute from "./routes/register.js";
 import loginRoute from "./routes/login.js";
+import reviewRoute from "./routes/review.js";
 import productRoute from "./routes/product.js";
+import { authMiddleware } from "./middleware/authMiddleware.js";
 import cors from "cors";
 
 import dotenv from "dotenv";
@@ -13,17 +15,25 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Conectado ao MongoDB");
 
-mongoose.connect(process.env.MONGO_URL);
+    app.use(authMiddleware);
+    app.use("/register", registerRoute);
+    app.use("/login", loginRoute);
+    app.use("/product", productRoute);
+    app.use("/review", reviewRoute);
 
-app.use("/register", registerRoute);
-app.use("/login", loginRoute);
-app.use("/product", productRoute);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Erro ao conectar ao MongoDB:", error);
+  });
